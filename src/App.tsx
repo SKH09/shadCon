5b3c8b8380734653a88b67534ee95a142047cccf
Khasim Hussain <khasimhussain.shaik@gmail.com>
2024-10-18 15:04:03 +0530

    seventh commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import "./App.css";
 import { Label } from "./components/ui/label";
 import { Input } from "./components/ui/input";
-import { RadioGroup, RadioGroupItem } from "./components/ui/radio-group";
-import { Checkbox } from "./components/ui/checkbox";
 import { Button } from "./components/ui/button";
 function App() {
   return (
