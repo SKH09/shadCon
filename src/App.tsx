5ba52b9bbdb17b1f3b9f29b9b52c25602895f104
Khasim Hussain <khasimhussain.shaik@gmail.com>
2024-10-18 15:01:08 +0530

    sixth commit

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,9 @@ function App() {
     <>
       <div className="font-robo">
         <div className="text-left">
-          <h2 className="font-extrabold text-3xl hover:text-green-700 cursor-pointer">Contact Us</h2>
+          <h2 className="font-extrabold text-3xl hover:text-green-700 cursor-pointer">
+            Contact Us
+          </h2>
         </div>
         {/* contact form */}
         <div className="">
@@ -18,12 +20,16 @@ function App() {
               {" "}
               {/* first name */}
               <div className="">
-                <Label className="text-xs">First Name <span className="text-[#0c7d69]">*</span></Label>
+                <Label className="text-xs">
+                  First Name <span className="text-[#0c7d69]">*</span>
+                </Label>
                 <Input type="text" />
               </div>
               {/* last name */}
               <div>
-                <Label className="text-xs">Last Name <span className="text-[#0c7d69]">*</span></Label>
+                <Label className="text-xs">
+                  Last Name <span className="text-[#0c7d69]">*</span>
+                </Label>
                 <Input type="text" />
               </div>
             </div>
@@ -38,46 +44,43 @@ function App() {
 
             {/* radio */}
             <div className="flex text-sm font-medium pt-4 ">
-              <p className="pb-4 text-xs">Query Type <span className="text-[#0c7d69]">*</span></p>
+              <p className="pb-4 text-xs">
+                Query Type <span className="text-[#0c7d69]">*</span>
+              </p>
             </div>
-            <RadioGroup name="myRadioGroup">
-              <div className="flex justify-around gap-4">
-                <div className="flex items-center gap-2 border-2 border-gray-200 w-full p-3 rounded-md">
-                  <RadioGroupItem
-                    value="general-enquiry"
-                    id="general-enquiry"
-                    className="rounded-full bg-white p-1"
-                  />
-                  <Label className="text-xs">General Enquiry</Label>
-                </div>
-                <div className="flex items-center gap-2 border-2 border-gray-200 w-full p-3 rounded-md">
-                  <RadioGroupItem
-                    value="support-request"
-                    id="support-request"
-                    className="rounded-full bg-white p-1"
-                  />
-                  <Label className="text-xs">Support Request</Label>
-                </div>
+
+            <div className="flex justify-around gap-4">
+              <div className="flex items-center gap-2 border-2 border-gray-200 w-full p-3 rounded-md">
+                <Input type="radio" name="queryType" className="w-4 h-auto " />
+                <Label className="text-xs">General Enquiry</Label>
               </div>
-            </RadioGroup>
+              <div className="flex items-center gap-2 border-2 border-gray-200 w-full p-3 rounded-md">
+                <Input type="radio" name="queryType" className="w-4 h-auto" />
+                <Label className="text-xs">Support Request</Label>
+              </div>
+            </div>
 
             {/* message */}
 
             <div className="flex flex-col items-start pt-4 gap-2">
-              <Label className="text-xs">Message <span className="text-[#0c7d69]">*</span></Label>
-              <Input type="text" className="py-12"/>
+              <Label className="text-xs">
+                Message <span className="text-[#0c7d69]">*</span>
+              </Label>
+              <Input type="text" className="py-12" />
             </div>
 
             {/* Checkbox */}
 
             <div className="flex gap-4 pt-4">
-              <Checkbox id="terms1" className="rounded-sm bg-white p-1" />
+              {/* <Checkbox id="terms1" className="rounded-sm bg-white p-1" /> */}
+              <Input type="checkbox" className="h-auto w-4" />
               <div className="flex items-center">
                 <label
                   htmlFor="terms1"
                   className="text-xs font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
                 >
-                  I consent to being contacted by the team  <span className="text-[#0c7d69]">*</span>
+                  I consent to being contacted by the team{" "}
+                  <span className="text-[#0c7d69]">*</span>
                 </label>
               </div>
             </div>
